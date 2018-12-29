203994df6edbe26d6008d1ca3df61bb9cf7cd8e2
Susheel Mandal <37742810+SUSHEELMANDAL@users.noreply.github.com>
2018-12-29 14:54:22 +0530

    Add files via upload

---
+++ Q20Susheel.py
@@ -0,0 +1,10 @@
+degree = lambda l: l.replace("a", "*").upper()
+
+print(degree("Susheel become a IT engineer in 2020 "))
+
+degree = lambda l: l.replace("a", "*").lower()
+
+print(degree("Susheel become a IT engineer in 2020"))
+
+
+
